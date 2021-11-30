97114946b494b3c04929f369c965df572211768d
Jorge Monge <jlo4321jl@gmail.com>
2021-11-29 21:49:05 -0600

    Add chackra provider

--- forkify_client/src/main.jsx
+++ forkify_client/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
+  <ChakraProvider>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ChakraProvider>,
   document.getElementById('root')
 )
