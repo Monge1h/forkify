12578e90a1015261fc91089d0e6b72e27d605f3d
Jorge Monge <jlo4321jl@gmail.com>
2021-11-29 20:47:31 -0600

    Add login route

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const express = require('express')
+const axios = require('axios')
+const querystring = require('querystring')
 const port = process.env.PORT || 8888
 
 const app = express()
@@ -11,6 +13,41 @@ const REDIRECT_URI = process.env.REDIRECT_URI
 app.get('/', (req, res) =>{
 	res.send("Hola")
 })
+
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+
+const stateKey = 'spotify_auth_state';
+
+app.get('/login', (req, res) => {
+	const state = generateRandomString(16);
+	res.cookie(stateKey, state);
+
+	const scope = 'user-read-private user-read-email';
+
+	const queryParams = querystring.stringify({
+	client_id: CLIENT_ID,
+	response_type: 'code',
+	redirect_uri: REDIRECT_URI,
+	state: state,
+	scope: scope,
+	});
+
+  	res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
 
 app.listen(port, ()=>{
 	console.log('http://localhost:'+port)
