e4465e7856a5ece4056d6ebc36acf6a7bde0393e
Jorge Monge <jlo4321jl@gmail.com>
2021-12-01 21:58:32 -0600

    Use react-router to render pages

--- forkify_client/src/App.jsx
+++ forkify_client/src/App.jsx
@@ -1,45 +1,31 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { Button, Heading, Link, VStack } from "@chakra-ui/react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	useLocation,
+} from "react-router-dom";
+import { getAccessToken } from "./spotify";
+import { CreatePlaylist, Home } from "./pages";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [token, setToken] = useState(null);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+	useEffect(() => {
+		setToken(getAccessToken);
+	}, []);
+
+	return (
+		<VStack>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home token={token} />} />
+					<Route path="/create-playlist" element={<CreatePlaylist />} />
+				</Routes>
+			</Router>
+		</VStack>
+	);
 }
 
-export default App
+export default App;
