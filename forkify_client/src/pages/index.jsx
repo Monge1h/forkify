e73a9e0fc6d856fdfbf79638882588a4cf2fbdb6
Jorge Monge <jlo4321jl@gmail.com>
2021-12-06 13:39:26 -0600

    export pages

--- forkify_client/src/pages/index.jsx
+++ forkify_client/src/pages/index.jsx
@@ -1,2 +1,6 @@
 export { CreatePlaylist } from "./CreatePlaylist";
 export { Home } from "./Home";
+export { SelectTerm } from "./SelectTerm"
+export { NumberOfSongs } from "./NumberOfSongs"
+export { SongsPreview } from "./SongsPreview"
+export { PlaylistLink } from "./PlaylistLink"
