98b24a534f11d1c20f61be64250d48ea487cdda7
Jorge Monge <jlo4321jl@gmail.com>
2021-12-07 10:16:10 -0600

    Change MONGO_URI value from index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URI = process.env.REDIRECT_URI
-const MONGO_URI = process.env.REDIRECT_URI
+const MONGO_URI = process.env.MONGO_URI
 
 mongoose.connect(MONGO_URI, {
 	useNewUrlParser: true,
