6061d31f391a95b9522a9b23b94b6f7bf9d91c21
Jorge Monge <jlo4321jl@gmail.com>
2021-12-25 09:57:48 -0600

    playlistlink styles

--- forkify_client/src/pages/PlaylistLink.jsx
+++ forkify_client/src/pages/PlaylistLink.jsx
@@ -4,12 +4,33 @@
  * @returns 
  */
 
+import { Container, Stack, Heading, Box, Link } from '@chakra-ui/react'
+
 function PlaylistLink({ playlistName, playlistIdMongo }) {
 	return (
-		<div>
-			<p>It's all done!{playlistName}</p>
-			<p>Share this link with your friends {playlistIdMongo}</p>
-		</div>
+      <Container 
+      height="100vh" 
+	  maxWidth="container.xl"
+	  display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection={"column"}
+      >
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+		  mb={50}
+          >
+          <Heading
+            fontWeight={800}
+            fontSize={{ base: '5xl', sm: '6xl', md: '7xl' }}
+            lineHeight={'110%'}>
+			It's all done!{playlistName}
+			Share this link with your friends {playlistIdMongo}
+          </Heading>
+        </Stack>
+      </Container>
 	)
 }
 
