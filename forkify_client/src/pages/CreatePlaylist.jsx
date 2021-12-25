73d3e81113166d0003f9b37c67f065f181d57bb4
Jorge Monge <jlo4321jl@gmail.com>
2021-12-24 18:31:02 -0600

    add style to create playlist page

--- forkify_client/src/pages/CreatePlaylist.jsx
+++ forkify_client/src/pages/CreatePlaylist.jsx
@@ -1,20 +1,53 @@
-import { Input, Button } from '@chakra-ui/react'
-import { Link } from 'react-router-dom';
+import { Input, Button, Container, Stack, Heading, Box, Link } from '@chakra-ui/react'
+
+import { Link as reactRouterLink } from "react-router-dom";
 
 
 function CreatePlaylist({ setPlaylistName }) {
 	return (
-		<div>
+      <Container 
+      height="100vh" 
+	  maxWidth="container.xl"
+	  display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection={"column"}
+      >
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+		  mb={50}
+          >
+          <Heading
+            fontWeight={800}
+            fontSize={{ base: '5xl', sm: '6xl', md: '7xl' }}
+            lineHeight={'110%'}>
+				Name of Playlist
+          </Heading>
+        </Stack>
 				<Input
 					type="text"
-					placeholder="name of playlist"
+					variant="filled"
+					width="lg"
+					size="lg"
 					onChange={(e) => setPlaylistName(e.target.value)}
 				/>
-				<Link to="/number-of-songs">
-					<Button colorScheme='pink' px='8' type='submit'>Next</Button>
-				</Link>
-		</div>
-	);
+              <Link as={reactRouterLink} to="/number-of-songs" mt={90}>
+					<Button
+						colorScheme={'green'}
+						bgGradient='linear(to-r, #2941AB, #034E0F)'
+						px={40}
+						_hover={{
+							bg: 'green.500',
+						}
+					}>
+					Next
+					</Button>
+              </Link>
+      </Container>
+	)
+	
 }
 
 export { CreatePlaylist };
