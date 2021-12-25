085e8be303db9d86f4a2f9d88a87865c35b6df0c
Jorge Monge <jlo4321jl@gmail.com>
2021-12-25 11:24:29 -0600

    Add style to playlist invitation

--- forkify_client/src/pages/PlaylistInvitation.jsx
+++ forkify_client/src/pages/PlaylistInvitation.jsx
@@ -1,8 +1,9 @@
-import { Link, Button, Heading } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useParams, Link as reactRouterLink } from "react-router-dom";
 import { getPlaylistId } from "../playlistBackend";
 import { getAccessToken, getCurrentUserData } from "../spotify";
+
+import {  Button, Container, Stack, Heading, Box, Link } from '@chakra-ui/react'
 
 
 function PlaylistInvitation({ setPlaylistId, token, setToken,setUserData }) {
@@ -26,19 +27,58 @@ function PlaylistInvitation({ setPlaylistId, token, setToken,setUserData }) {
 	}, [])
 
 	return (
-		<div>
-			te han invitado a poner canciones en esta playlist
-			{!token ? (
-				<Link mt={90} href="http://localhost:8888/login">
-					<Button>Login</Button>
-				</Link>
-			) : (
-				<Link as={reactRouterLink} mt={90} to="/number-of-songs">
-					<Button>Continuar</Button>
-				</Link>
-			)}
-			xd
-		</div>
+      <Container 
+      height="100vh" 
+	    maxWidth="container.xl"
+	    display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection={"column"}
+      >
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          >
+          <Heading
+            fontWeight={800}
+            fontSize={{ base: '5xl', sm: '6xl', md: '7xl' }}
+            lineHeight={'110%'}>
+			You have been invited to put songs on this playlist
+          </Heading>
+        </Stack>
+          <Stack
+            direction={'column'}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            {!token ? (
+              <Link mt={90} href="http://localhost:8888/login">
+                <Button
+                  colorScheme={'green'}
+                  bgGradient='linear(to-r, #2941AB, #034E0F)'
+                  px={40}
+                  _hover={{
+                    bg: 'green.500',
+                  }}>
+                  Log in with Spotify
+                </Button>
+              </Link>
+            ) : (
+              <Link mt={90} to="/number-of-songs" as={reactRouterLink}>
+                <Button
+                  colorScheme={'green'}
+                  bgGradient='linear(to-r, #2941AB, #034E0F)'
+                  px={40}
+                  _hover={{
+                    bg: 'green.500',
+                  }}>
+                  Next
+                </Button>
+              </Link>
+            )}
+          </Stack>
+      </Container>
 	)
 }
 
